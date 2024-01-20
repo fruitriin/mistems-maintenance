d703d504625ccded8d257770c0c6b9e777c41c70
FruitRiin <nassii74@gmail.com>
2024-01-20 17:30:21 +0900

    iframe

--- app.vue
+++ app.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
+  <div class="app">
     Mistems is down
 
     <iframe src="https://status.misskey.systems/" width="100%" height="100%"></iframe>
   </div>
 </template>
+
+<style>
+html, body ._nuxt, .app {
+  height: 100%;
+}
+</style>
