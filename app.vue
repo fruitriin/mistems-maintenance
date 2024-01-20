4894b7fdb6d2fc92da22516667f47b47c352f5f6
FruitRiin <nassii74@gmail.com>
2024-01-20 18:31:43 +0900

    fix

--- app.vue
+++ app.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="app">
-    Mistems is down
+  <div class="app" style="display: flex; flex-direction: column; overflow: hidden;">
+    <div>
+      <h1>Mistems is down</h1>
 
+      <a href="https://misskey.systems/">みすてむずは直ってるに違いないときに押すリンク</a>
+    </div>
     <iframe src="https://status.misskey.systems/" width="100%" height="100%"></iframe>
   </div>
 </template>
@@ -11,3 +14,5 @@ html, body ,#__nuxt, .app {
   height: 100%;
 }
 </style>
+<script setup lang="ts">
+</script>
