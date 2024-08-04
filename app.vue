ab730aa94ade8d94147bf41c7de8e13d9e8773a4
FruitRiin <nassii74@gmail.com>
2024-08-04 12:57:43 +0900

    perfomance metric

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     class="app"
-    style="display: flex; flex-direction: column; overflow: hidden"
+    style="display: flex; flex-direction: column; overflow-y: hidden"
   >
     <div style="display: flex">
       <div>
@@ -17,31 +17,113 @@
         </form>
         <ul>
           <li v-for="chat in chats" :key="chat.id">
-            {{
-              formatDistanceToNow(chat.created_at, {
-                includeSeconds: true,
-                locale: ja(),
-              })
-            }}
+            <span class="min-width">
+              {{
+                formatDistanceToNow(chat.created_at, {
+                  includeSeconds: true,
+                  locale: ja(),
+                })
+              }}
+            </span>
             {{ chat.message }}
           </li>
         </ul>
       </div>
     </div>
+    <div>
+      <button
+        @click="tab = 'status'"
+        class="tab"
+        :class="{ 'tab-active': tab === 'status' }"
+      >
+        障害状況
+      </button>
+      <button
+        @click="tab = 'metric'"
+        class="tab"
+        :class="{ 'tab-active': tab === 'metric' }"
+      >
+        サーバーパフォーマンスモニタ
+      </button>
+    </div>
     <iframe
+      v-if="tab === 'status'"
       src="https://status.misskey.systems/"
       width="100%"
       height="100%"
     ></iframe>
+
+    <div
+      v-if="tab === 'metric'"
+      class="metric"
+      style="display: flex; flex-wrap: wrap"
+    >
+      <div class="item">
+        ロードアベレージ<br />
+        <iframe
+          src="https://mackerel.io/embed/public/embed/zDhYnIFK8HaOF8XjzPw5mWhrb4QJTFpj5UhNdoIMiBB04IJcEisyNdBRXdKJ2fq1?period=30m"
+          height="200"
+          width="400"
+          frameborder="0"
+        />
+      </div>
+      <div class="item">
+        CPU<br />
+        <iframe
+          src="https://mackerel.io/embed/public/embed/EA0rfRa2rfdRVbQ7DISMyAjGMCtU36l84tdOerjrM5t13ydZiARjh1iuze6QxEAr?period=30m"
+          height="200"
+          width="400"
+          frameborder="0"
+        />
+      </div>
+      <div class="item">
+        メモリー<br />
+        <iframe
+          src="https://mackerel.io/embed/public/embed/iqmXxYziC63FR1XX7mZUOjR1KzGsRwNnTXHGpzUj7PmschRVIek6VPt6FrbZkB9R?period=30m"
+          height="200"
+          width="400"
+          frameborder="0"
+        />
+      </div>
+      <div class="item">
+        Disk<br />
+        <iframe
+          src="https://mackerel.io/embed/public/embed/Vup7uXZC6PtSia2kv4rnOti2weHt6i4qckjSavZdPBk2sMBcxvyJtXTILFoxAoDq?period=30m"
+          height="200"
+          width="400"
+          frameborder="0"
+        ></iframe>
+      </div>
+      <div class="item">
+        IO<br />
+        <iframe
+          src="https://mackerel.io/embed/public/embed/x7at7iB6tpbxul9DlhvYOiL63MZUa3TJVjREn390BSKdma05weDmTv2j1wlYEf0U?period=30m"
+          height="200"
+          width="400"
+          frameborder="0"
+        ></iframe>
+      </div>
+    </div>
   </div>
 </template>
 
-<style>
+<style lang="scss">
 html,
 body,
 #__nuxt,
 .app {
   height: 100%;
+}
+.time {
+  min-width: 100px;
+}
+
+.tab {
+  border-radius: 4px;
+  padding: 4px 8px;
+  &.tab-active {
+    background-color: rgb(134, 179, 0);
+  }
 }
 </style>
 <script lang="ts">
@@ -59,6 +141,7 @@ const supabase = createClient(
 export default defineNuxtComponent({
   data() {
     return {
+      tab: "status" as "status" | "metric",
       chats: [] as { id: number; message: string; created_at: Date }[],
       text: "",
     };
